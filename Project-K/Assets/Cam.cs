36ea329c46bc35c9258070f94fd61554246b182a
CodeX-cmd <64374046+CodeX-cmd@users.noreply.github.com>
2020-04-27 13:38:09 +0200

    Kretanje kamere, par stvari pocisceno

---
+++ Project-K/Assets/Cam.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cam : MonoBehaviour {
+	public float xRotate = 0f;
+	public float yRotate = 0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		xRotate += Input.GetAxis ("Mouse X");
+		yRotate -= Input.GetAxis ("Mouse Y");
+	}
+
+}
--- Project-K/Assets/Player.cs
+++ Project-K/Assets/Player.cs
@@ -8,9 +8,16 @@ using UnityEngine.Events;
 public class Player : MonoBehaviour {
 	UnityEvent keyEvent;
 	float speed = 0.1f;
-
+	float targetSpeed;
+	float t = 0.6f;
+	Vector3 lastMousePos;
+	Cam cam;
 	// Use this for initialization
 	void Start () {
+		lastMousePos = Input.mousePosition;
+		//cam  = FindObjectOfType<Camera> ();
+		cam = transform.GetComponentInChildren<Cam> ();
+		targetSpeed = this.speed;
 		if (keyEvent == null) {
 			keyEvent = new UnityEvent ();
 		}
@@ -19,25 +26,39 @@ public class Player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		updateCam ();
 		if (Input.anyKey && keyEvent != null) {
 			
 			keyEvent.Invoke ();
+		} else {
+			this.speed = 0f;
+			this.t = 0.6f;
 		}
 	
 	}
+	void updateCam(){
+		cam.transform.SetPositionAndRotation (this.transform.position, Quaternion.identity);
+		cam.transform.eulerAngles = new Vector3(cam.yRotate, cam.xRotate, 0.0f);
+		lastMousePos = Input.mousePosition;
+	}
 	void movement(){
+		this.speed = Mathf.Lerp (0.2f, targetSpeed, t);
+		t = 0.6f * Time.deltaTime;
+
+			//Vector3.Angle (lastMousePos, Input.mousePosition);
+	
 		if (Input.GetKey ("d")) {
-			this.transform.SetPositionAndRotation (new Vector3 (this.transform.position.x + speed * (Mathf.Sqrt(2) - 1), this.transform.position.y, this.transform.position.z), Quaternion.identity);
+			this.transform.SetPositionAndRotation (new Vector3 (this.transform.position.x + this.speed * (Mathf.Sqrt(2) - 1), this.transform.position.y, this.transform.position.z), Quaternion.identity);
 			if (Input.GetKey ("w")) {
-				this.transform.SetPositionAndRotation (new Vector3 (this.transform.position.x, this.transform.position.y, this.transform.position.z + speed), Quaternion.identity);
-				this.transform.SetPositionAndRotation (new Vector3 (this.transform.position.x + speed, this.transform.position.y, this.transform.position.z), Quaternion.identity);
+				this.transform.SetPositionAndRotation (new Vector3 (this.transform.position.x, this.transform.position.y, this.transform.position.z + this.speed), Quaternion.identity);
+				this.transform.SetPositionAndRotation (new Vector3 (this.transform.position.x + this.speed , this.transform.position.y, this.transform.position.z), Quaternion.identity);
 				return;
 			} else if (Input.GetKey ("s")) {
-				this.transform.SetPositionAndRotation (new Vector3 (this.transform.position.x, this.transform.position.y, this.transform.position.z - speed), Quaternion.identity);
-				this.transform.SetPositionAndRotation (new Vector3 (this.transform.position.x + speed, this.transform.position.y, this.transform.position.z), Quaternion.identity);
+				this.transform.SetPositionAndRotation (new Vector3 (this.transform.position.x, this.transform.position.y, this.transform.position.z - this.speed), Quaternion.identity);
+				this.transform.SetPositionAndRotation (new Vector3 (this.transform.position.x + this.speed, this.transform.position.y, this.transform.position.z), Quaternion.identity);
 				return;
 			}
-			this.transform.SetPositionAndRotation (new Vector3 (this.transform.position.x + speed, this.transform.position.y, this.transform.position.z), Quaternion.identity);
+			this.transform.SetPositionAndRotation (new Vector3 (this.transform.position.x + this.speed, this.transform.position.y, this.transform.position.z), Quaternion.identity);
 		} else if (Input.GetKey ("a")) {
 			this.transform.SetPositionAndRotation (new Vector3 (this.transform.position.x - speed * (Mathf.Sqrt(2) - 1), this.transform.position.y, this.transform.position.z), Quaternion.identity);
 			if (Input.GetKey ("w")) {
@@ -57,5 +78,6 @@ public class Player : MonoBehaviour {
 			this.transform.SetPositionAndRotation (new Vector3 (this.transform.position.x, this.transform.position.y, this.transform.position.z + 2*speed), Quaternion.identity);
 		}
 
+	
 	}
 }
